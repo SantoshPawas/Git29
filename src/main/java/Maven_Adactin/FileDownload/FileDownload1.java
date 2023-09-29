6f5ef0d73f55161476b514974c64bc78addd3b08
SantoshPawas <santoshpawase531@gmail.com>
2023-09-29 09:36:40 -0700

    Todays commit

---
+++ src/main/java/Maven_Adactin/FileDownload/FileDownload1.java
@@ -0,0 +1,40 @@
+package Maven_Adactin.FileDownload;
+
+import java.time.Duration;
+import java.util.HashMap;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+
+public class FileDownload1 
+{
+	public static void main(String[] args) 
+	{
+		ChromeOptions co=new ChromeOptions();
+		String s1=System.getProperty("user.dir")+"\\All_Download\\";
+		//this is default directory where we have to stoare our file
+		
+		HashMap mp=new HashMap();
+		mp.put("download.default_directory", s1);
+		
+		//set chrome browser
+		
+		co.setExperimentalOption("prefs", mp);
+		
+		WebDriver driver=new ChromeDriver(co);
+		
+		driver.get("https://chromedriver.storage.googleapis.com/index.html?path=114.0.5735.90/");
+		
+		driver.manage().window().maximize();
+		
+		//wait
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		
+		driver.findElement(By.xpath("//*[text()='chromedriver_win32.zip']")).click();
+	
+		
+	}
+}
